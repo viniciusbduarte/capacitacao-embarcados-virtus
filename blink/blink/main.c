716b29c48fad47fdf6f62f3be5e7041b3e2ec9e2
viniciusbduarte <vg39348@gmail.com>
2026-01-20 23:11:15 -0300

    create/blink

--- blink/blink/main.c
+++ blink/blink/main.c
@@ -5,19 +5,20 @@
  * Author : Vinicius Duarte
  */ 
 
+#define F_CPU 16000000UL
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
-	DDRB = 0b00100000; //Habilita o pino PB5 como saída 
-
-    while (1) 
-    {
-		PORTB = 0b00100000;
-		PORTB = 0b00000000;
-
-		
-    }
+	/* Replace with your application code */
+	DDRB |= (1 << 5);   // PB5 como saída
+	
+	while (1)
+	{
+		PORTB = 0b00100000; //Pino PB5 em nível alto
+		_delay_ms(1000);
+		PORTB = 0b00000000; //Pino PB5 em nível baixo
+		_delay_ms(1000);
+	}
 }
-
